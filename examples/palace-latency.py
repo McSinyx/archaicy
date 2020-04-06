ce183b7cb691c661177a9dd6d40bef35a749a165
Huy Ngo <duchuy29092000@gmail.com>
2020-04-06 17:05:08 +0700

    Resolve requests and fix formatting error

--- examples/palace-latency.py
+++ examples/palace-latency.py
@@ -18,38 +18,41 @@
 # along with palace.  If not, see <https://www.gnu.org/licenses/>.
 
 from argparse import ArgumentParser
-from datetime import timedelta
 from itertools import count, takewhile
-from sys import exit
+from sys import stderr
 from time import sleep
+from typing import Iterable
 
-from palace import (
-    Buffer, Context, Device, Source,
-    decode)
+from palace import Context, Device, Source, decode
 
 CHUNK_LEN: int = 12000
-QUEUE_SIZE:int = 40
+QUEUE_SIZE: int = 40
 PERIOD: float = 0.01
 
 
-def play(filename: str, device: str) -> None:
+def play(files: Iterable[str], device: str) -> None:
     """Load and play the file on given device."""
-    with Device(device) as dev, Context(dev) as ctx, Buffer(filename) as buf, Source() as src:
-        decoder = decode(filename)
-        decoder.play(CHUNK_LEN, QUEUE_SIZE, src)
-        for i in takewhile(lambda i: src.playing, count()):
-            print(f'Latency at {round(i * PERIOD, 2)}s: ', src.latency, '(ns)')
-            sleep(PERIOD)
-            ctx.update()
+    with Device(device) as dev, Context(dev) as ctx, Source() as src:
+        for filename in files:
+            try:
+                decoder = decode(filename)
+            except RuntimeError:
+                stderr.write(f'Failed to open file: {filename}\n')
+            decoder.play(CHUNK_LEN, QUEUE_SIZE, src)
+            print('Playing: ', filename)
+            for i in takewhile(lambda i: src.playing, count()):
+                print('Offset:', round(src.offset_seconds),
+                      '-',
+                      'Latency:', src.latency//10**6, 'ms',
+                      end='\r', flush=True)
+                sleep(PERIOD)
+                ctx.update()
         print()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('filename', help='audio files')
+    parser.add_argument('files', nargs='+', help='audio files')
     parser.add_argument('-d', '--device', default='', help='device name')
     args = parser.parse_args()
-    filename = args.filename
-    device = args.device
-    play(filename, device)
-    
+    play(args.files, args.device)
