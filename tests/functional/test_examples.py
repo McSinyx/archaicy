59d21008d64938b179fb894c68f5ea12644ae3de
Huy Ngo <duchuy29092000@gmail.com>
2020-04-21 21:49:50 +0700

    Use join path and fix some typos

--- tests/functional/test_examples.py
+++ tests/functional/test_examples.py
@@ -16,28 +16,29 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with palace.  If not, see <https://www.gnu.org/licenses/>.
 
+from os.path import abspath, basename, join
 from subprocess import run
 from sys import executable
 
-EVENT = './palace-event.py'
-HRTF = './palace-hrtf.py'
-INFO = './palace-info.py'
-LATENCY = './palace-latency.py'
-REVERB = '/palace-reverb.py'
-STDEC = './palace-stdec.py'
-TONEGEN = '/palace-tonegen.py'
+EVENT = join(abspath(basename('./palace-event.py')))
+HRTF = join(abspath(basename('./palace-hrtf.py')))
+INFO = join(abspath(basename('./palace-info.py')))
+LATENCY = join(abspath(basename('./palace-latency.py')))
+REVERB = join(abspath(basename('./palace-reverb.py')))
+STDEC = join(abspath(basename('./palace-stdec.py')))
+TONEGEN = join(abspath(basename('./palace-tonegen.py')))
 WAVEFORMS = ['sine', 'square', 'sawtooth',
              'triangle', 'impulse', 'white-noise']
 
 
 def test_event():
-    event = run([executable, EVENT, WAV], capture_output=True)
+    event = run([executable, EVENT, WAV], capture_output=True)  # noqa
     assert 'Opened' in event.stdout
     assert 'Playing' in event.stdout
 
 
 def test_hrtf():
-    hrtf = run([executable, HRTF, WAV], capture_output=True)
+    hrtf = run([executable, HRTF, WAV], capture_output=True)  # noqa
     assert 'Opened' in hrtf.stdout
     assert 'Playing' in hrtf.stdout
 
@@ -56,7 +57,7 @@ def test_info():
 
 
 def test_latency():
-    latency = run([executable, LATENCY, WAV], capture_output=True)
+    latency = run([executable, LATENCY, WAV], capture_output=True)  # noqa
     assert 'Opened' in latency.stdout
     assert 'Playing' in latency.stdout
     assert 'Offset' in latency.stdout
@@ -67,14 +68,14 @@ def test_reverb():
     assert 'Available reverb preset names:' in reverbs.stdout
     fxs = reverbs.stdout.split('\n')[1:]
     for fx in fxs:
-        reverb = run([executable, REVERB, '-r', fx], capture_output=True)
+        reverb = run([executable, REVERB, '-r', fx, WAV], capture_output=True)  # noqa
         assert 'Opened' in reverb.stdout
         assert 'Playing' in reverb.stdout
         assert fx in reverb.stdout
 
 
 def test_stdec():
-    stdec = run([executable, STDEC, WAV], capture_output=True)
+    stdec = run([executable, STDEC, WAV], capture_output=True)  # noqa
     assert 'Opened' in stdec.stdout
     assert 'Playing' in stdec.stdout
 
